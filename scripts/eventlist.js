ecef9578af1bb81b19314273e5f543e43f2b1ec2
Ivo <git@naepflin.com>
2014-11-24 20:49:05 +0100

    added filtering by date

--- scripts/eventlist.js
+++ scripts/eventlist.js
@@ -2,14 +2,31 @@ angular.module('eventlist',[]);
 	
 	
 function eventlistController($scope, $http) {
-	$scope.eventlist = [];
+	$scope.futureEventlist = [];
+	$scope.nextEventlist = [];
+	$scope.pastEventlist = [];
 	
 	$http.get('events.json').then(
 	function (response) {
-		$scope.eventlist = response.data;
-		$scope.eventlist.sort(function(a, b) {
-			return new Date(a.date) > new Date(b.date);
+		var eventlist = response.data;
+		eventlist.sort(function(a, b) {
+			return new Date(a.date) < new Date(b.date);
 		});
+		var today = new Date();
+		
+		$scope.futureEventlist = eventlist.filter(function(a) {
+			return new Date(a.date) >= today;
+		});
+		
+		$scope.pastEventlist = eventlist.filter(function(a) {
+			return new Date(a.date) < today;
+		});
+
+		
+		
+		$scope.nextEventlist = [$scope.futureEventlist.pop()];
+		
+
 		
 	});
 };
