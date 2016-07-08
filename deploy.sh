88d9cfe237a2fe0469697ae5ec19c48ce27e7058
Markus Schlichting <comment@mynethome.de>
2016-07-09 00:41:17 +0200

    remove gh_pages branch handling from deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
-TARGET_BRANCH="gh_pages"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
@@ -15,15 +14,6 @@ fi
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
-
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-
-# Run our compile script
-gulp
 
 # Now let's go have some fun with the cloned repo
 git config user.name "Travis CI"
@@ -51,4 +41,4 @@ eval `ssh-agent -s`
 ssh-add deploy_key
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git push $SSH_REPO 
