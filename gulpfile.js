deae14ff608b5d296e3abbf6684150990442a0c5
Marcus Fihlon <marcus@fihlon.ch>
2018-08-07 16:37:00 +0200

    :bug: Fixing feed generation after event format change

--- gulpfile.js
+++ gulpfile.js
@@ -48,8 +48,13 @@ gulp.task('default', function() {
 			for (var i = 0; i < events.length; i++) {
 				var event = events[i];
 				var hash = hashCode(event.date + event.location);
+				if (event.title && event.location) { // old JSON format without venue and address
+					event.title = "on " + event.date + " at " + event.title + " in " + event.location;
+				} else { // new JSON format with venue and address
+					event.title = "on " + event.date + " at " + event.venue + ", " + event.address;
+				}
 				xml.push("<item>");
-				xml.push("<title>" + escapeHTML("Hackgarten at " + event.title + " on " + event.date + " in " + event.location) + "</title>");
+				xml.push("<title>" + escapeHTML("Hackgarten " + event.title) + "</title>");
 				xml.push("<link><a href=\"http://hackergarten.net/#event-" + hash + "\"></a></link>");
 				xml.push("<guid>" + hash + "</guid>");
 				xml.push("</item>\n");
