f5a6125f0f00211336098ed57fdbe7bd9674d407
Marcus Fihlon <marcus@fihlon.ch>
2018-10-06 11:19:55 +0200

    :bug: Fixing possible coding problems

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ var rename = require('gulp-rename');
 
 var hashCode = function (str) {
 	var hash = 0;
-	if (str.length == 0) return hash;
+	if (str.length === 0) return hash;
 	for (var i = 0; i < str.length; i++) {
 		var char = str.charCodeAt(i);
 		hash = ((hash<<5)-hash)+char;
 		hash = hash & hash; // Convert to 32bit integer
 	}
 	return Math.abs(hash);
-}
+};
 
 var entityMap = {
 	"&": "&amp;",
